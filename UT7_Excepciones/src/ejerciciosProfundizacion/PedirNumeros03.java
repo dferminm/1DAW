be1a9fcd8366cfc892a902e0cf28eede2ef9e5e2
David <David@David-PC>
2015-02-11 19:54:10 +0100

    Excepciones - Profundización

--- UT7_Excepciones/src/ejerciciosProfundizacion/PedirNumeros03.java
+++ UT7_Excepciones/src/ejerciciosProfundizacion/PedirNumeros03.java
@@ -1,10 +1,18 @@
 package ejerciciosProfundizacion;
 
+import java.util.*;
+
 public class PedirNumeros03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Escribe un programa que se encargue de pedir números 
+		// hasta que se indique un número comprendido entre 1 y 5 (ambos inclusive), 
+		// visualizando al final del programa un mensaje como este: "Número encontrado". 
+		// Hay que validar que el usuario no inserte letras.
 
+		Scanner teclado = new Scanner(System.in);
+		
+		
 	}
 
 }
