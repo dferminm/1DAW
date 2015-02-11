44e524c6c6d292e42f891136952c7b9c2081a05c
David <David@David-PC>
2015-02-11 20:15:16 +0100

    Excepciones

---
+++ UT7_Excepciones/src/ejerciciosProfundizacion/DiezNumDecimales04.java
@@ -0,0 +1,21 @@
+package ejerciciosProfundizacion;
+
+import java.util.*;
+
+public class DiezNumDecimales04 {
+
+	public static void main(String[] args) {
+		// Escribe un programa que pida diez números con decimales. 
+		// Si insertan letras, se pedirá de nuevo dicho número. 
+		// El programa debe visualizar el número más alto de todos los insertados. 
+		// Si se produce alguna excepción se mostrará el mensaje correspondiente. 
+		// Además, al final del programa se visualizará un mensaje que indica 
+		// cuántas veces se ha producido dicho error.
+
+		Scanner teclado = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
