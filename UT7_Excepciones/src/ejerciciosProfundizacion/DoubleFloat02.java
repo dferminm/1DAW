638aef8b765a33c4874f6a0f4c10486b3cafe922
David <David@David-PC>
2015-02-11 11:22:39 +0100

    Excepciones - Profundización

---
+++ UT7_Excepciones/src/ejerciciosProfundizacion/DoubleFloat02.java
@@ -0,0 +1,61 @@
+package ejerciciosProfundizacion;
+
+import java.util.*;
+
+import ejemplos.Teclado;
+
+public class DoubleFloat02 {
+
+	public static void main(String[] args) {
+		// Escribe un programa que pida un double y un float. 
+		// Si al pedir un número, el usuario inserta letras, se pedirá de nuevo ese dato. 
+		// Una vez insertados, se visualizará el número más alto de los dos. 
+		// Si son iguales se visualizará cualquiera de ellos. 
+		// Se usará un método que pida un dato de tipo float. 
+		// Dicho método controlará el error que se podría producir si se insertan letras, 
+		// visualizando en este caso un mensaje de error. 
+		// El main será el que se encargue de llamar de nuevo a este método si se insertan letras. 
+		// De la misma forma se hará a la hora de pedir un dato de tipo double.
+
+		boolean valido = false;
+		
+		do {
+		try {
+			numeroFloat();
+			valido = true;
+		}
+		catch (InputMismatchException imex) {
+		}
+		}while(!valido);
+	}
+
+	public static float numeroFloat () {
+		Scanner teclado = new Scanner(System.in);
+		
+		float n = 0;
+		
+		try {
+			System.out.println("Dime un número float: ");
+			n = teclado.nextFloat();
+			}
+			catch (InputMismatchException imex) {
+				System.out.println("Error. Eso no es un dato de tipo float.");
+			}
+		return n; 
+	}
+	
+	public static double numeroDouble () {
+		Scanner teclado = new Scanner(System.in);
+		
+		double n = 0;
+		
+		try {
+			System.out.println("Dime un número double: ");
+			n = teclado.nextDouble();
+		}
+		catch (InputMismatchException imex) {
+			System.out.println("Error. Eso no es un dato de tipo double.");
+		}
+		return n;
+	}
+}
