dfca9d9562da68bbf5d6edcd407c045f0f77be3c
David <David@David-PC>
2015-02-11 20:08:59 +0100

    Excepciones

--- UT7_Excepciones/src/ejerciciosProfundizacion/PedirNumeros03.java
+++ UT7_Excepciones/src/ejerciciosProfundizacion/PedirNumeros03.java
@@ -12,7 +12,21 @@ public class PedirNumeros03 {
 
 		Scanner teclado = new Scanner(System.in);
 		
+		int n = 0;
 		
+		while(n < 1 || n > 5) {
+		try {
+			System.out.println("Introduzca un número: ");
+			n = teclado.nextInt();
+			if(n >= 1 && n <= 5) {
+				System.out.println("Número encontrado.");
+				System.exit(0);
+			}
+		}catch (InputMismatchException imex) {
+			System.out.println("Eso no es un número.");
+			teclado.next();
+		}
+		}
 	}
 
 }
