3ca3a20a890cdb64f7f32e4de4c692d04525a07e
David Fermín Maestro <davidfermin8@gmail.com>
2015-03-17 10:27:11 +0100

    clases

--- Programacion/UT8_Clases/src/ejercicios/bloque3/filmoteca/Filmoteca.java
+++ Programacion/UT8_Clases/src/ejercicios/bloque3/filmoteca/Filmoteca.java
@@ -1,10 +1,18 @@
 package ejercicios.bloque3.filmoteca;
 
 import java.util.ArrayList;
+import ejercicios.bloque3.filmoteca.*;
 
 public class Filmoteca {
 	
-	//  insertar, modificar, eliminar, buscar o visualizar una película.
+	/* Imagina que quieres crear un programa que te permita organizar tus películas. 
+	Crearemos una clase Película donde recogemos toda la información relacionada con un film. 
+	Algunos campos serán Titulo, Actores (array de nombres), Directores (array de nombres), 
+	fecha estreno, formato, etc. 
+	Crea un programa (clase Filmoteca) que permita: 
+	insertar, modificar, eliminar, buscar o visualizar una película.*/
+	
+	Pelicula pelicula = new Pelicula();
 	
 	
 }
--- Programacion/UT8_Clases/src/ejercicios/bloque3/filmoteca/Pelicula.java
+++ Programacion/UT8_Clases/src/ejercicios/bloque3/filmoteca/Pelicula.java
@@ -2,31 +2,91 @@ package ejercicios.bloque3.filmoteca;
 
 import java.util.ArrayList;
 
+class Peliculas {
+	
+	private String codigo;
+
+	public Peliculas(String codigo) {
+		super();
+		this.codigo = codigo;
+	}
+
+	@Override
+	public String toString() {
+		return "Peliculas [codigo=" + codigo + "]";
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Peliculas other = (Peliculas) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+}
+
 public class Pelicula {
 	
-	/* Algunos campos serán Titulo, Actores (array de nombres), 
-	Directores (array de nombres), fecha estreno, formato, etc.*/
-	
-	//  insertar, modificar, eliminar, buscar o visualizar una película.
-	
 	private String titulo, fechaEstreno, formato;
-	private ArrayList <String> actores, directores;
+	private ArrayList <Peliculas> peliculas;
 	
-	public Pelicula() {
+	public Pelicula () {
+		this.titulo = null;
+		this.fechaEstreno = null;
+		this.formato = null;
+		peliculas = new ArrayList<Peliculas>();
+	}
+	
+	public Pelicula(String titulo, String fechaEstreno, String formato,
+			ArrayList<String> actores, ArrayList<String> directores) {
 		super();
 		this.titulo = titulo;
 		this.fechaEstreno = fechaEstreno;
 		this.formato = formato;
-		actores = new ArrayList<String>();
-		directores = new ArrayList<String>();
+	}
+
+	public void insertarPelicula (Peliculas pelicula) {
+		this.peliculas.add(pelicula);
 	}
 	
+	public void eliminarPelicula (Peliculas pelicula) {
+		this.peliculas.remove(pelicula);
+	}
+	
+	public void buscarPelicula (Peliculas pelicula) {
+		this.peliculas.contains(pelicula);
+	}
 	
 	@Override
 	public String toString() {
 		return " Titulo: " + titulo + "\n fechaEstreno: " + fechaEstreno
-				+ "\n formato: " + formato + "\n actores: " + actores
-				+ "\n directores: " + directores;
+				+ "\n formato: " + formato;
 	}
 	
 }
